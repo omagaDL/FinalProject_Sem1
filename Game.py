cc347a57c48bf985cd21553f9c1fb6081c41ddcb
maximpermiakov <88048869+maximpermiakov@users.noreply.github.com>
2022-12-19 02:54:08 +0300

    Выбор уровня, доработанное появление монстров

--- Game.py
+++ Game.py
@@ -16,14 +16,13 @@ clock = pygame.time.Clock()  # вот тут вот вообще лучше ни
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("The_deadly_wave")
 FPS = 50
-BACK = ['fon.png', "hero_choose.jpg", 'game_fon_1', 'game_fon']  # здесь будут фоны
+BACK = ['fon.png', 'game_fon_1.jpg', 'game_fon_2.jpg', 'game_fon_3.jpg']  # здесь будут фоны
 sound = 0
 phase = 'start_screen'
 all_sprites = pygame.sprite.Group()
 death = pygame.mixer.Sound('data_death.wav')
 hero = None
 time_ = 0
-time_record = 0
 time_start = 0
 new_game = 1
 was_paused = False
@@ -35,6 +34,8 @@ chx = 0
 chy = 0
 monsta = list()
 game_start = False
+levelnumber = 1
+records = [0, 0, 0]
 
 
 COLORS = [RED, BRIGHTRED, GREEN, BRIGHTGREEN] = [(220, 0, 0), (255, 0, 0), (100, 200, 100), (100, 255, 100)]
@@ -65,12 +66,12 @@ def camera_configure(camera, target_rect):
     return Rect(l, t, w, h)
 
 
-def loadLevel():
+def loadLevel(levelnumber):
     global playerX, playerY, level_h, level_w, platforms, level  # объявляем глобальные переменные, это координаты героя
 
     level = []
     platforms = []
-    levelFile = open('%s/levels/1.txt' % FILE_DIR)
+    levelFile = open(f'%s/levels/{levelnumber}.txt' % FILE_DIR)
     line = " "
     while line[0] != "/":  # пока не нашли символ завершения файла
         line = levelFile.readline()  # считываем построчно
@@ -200,25 +201,30 @@ def start_screen():
                         sound = 1
                     else:
                         sound = 0
-                elif button('Новая игра', width // 2, height // 2 - 50, event):
+                elif button('Новая игра', width // 2, height // 2 - 100, event):
                     phase = 'gameplay'
                     return
                 elif button('Выход', width // 2, height - 50, event):
                     terminate()
-                elif button('Выбор героя', width // 2, height // 2 + 50, event):
+                elif button('Выбор уровня', width // 2, height // 2 - 20, event):
+                    phase = 'level_choice'
+                    return
+                elif button('Выбор героя', width // 2, height // 2 + 60, event):
                     phase = 'hero_choice'
                     return
-                elif button('Выбор управления', width // 2, height // 2 + 150, event):
+                elif button('Выбор управления', width // 2, height // 2 + 140, event):
                     phase = 'control_choice'
                     return
         if sound == 0 and pygame.mixer.music.get_busy():
             pygame.mixer.music.stop()
         if sound == 1 and not pygame.mixer.music.get_busy():
             pygame.mixer.music.play(-1)
-        text_render('Новая игра', width // 2, height // 2 - 50, GREEN, BRIGHTGREEN, mousepos)
-        text_render('Выход', width // 2, height - 50,RED, BRIGHTRED,  mousepos)
-        text_render('Выбор героя', width // 2, height // 2 + 50,GREEN, BRIGHTGREEN,  mousepos)
-        text_render('Выбор управления', width // 2, height // 2 + 150,GREEN, BRIGHTGREEN, mousepos)
+        text_render('Новая игра', width // 2, height // 2 - 100, GREEN, BRIGHTGREEN, mousepos)
+        text_render('Выход', width // 2, height - 50, RED, BRIGHTRED,  mousepos)
+        text_render('Выбор уровня', width // 2, height // 2 - 20, GREEN, BRIGHTGREEN, mousepos)
+        text_render('Выбор героя', width // 2, height // 2 + 60, GREEN, BRIGHTGREEN,  mousepos)
+        text_render('Выбор управления', width // 2, height // 2 + 140, GREEN, BRIGHTGREEN, mousepos)
+
         if sound == 1:
             if (width - 50 < mousepos[0] < width - 10) and (
                     10 < mousepos[1] < 50):
@@ -235,11 +241,11 @@ def start_screen():
 
 
 def score():
-    global phase, time_, time_record, new_game, mousepos, game_start
+    global phase, time_, records, new_game, mousepos, game_start, levelnumber
     game_start = False
     new_game = 1
-    if int(time_) > time_record:
-        time_record = int(time_)
+    if int(time_) > records[levelnumber - 1]:
+        records[levelnumber - 1] = int(time_)
         record = True
     else:
         record = False
@@ -250,9 +256,9 @@ def score():
     screen.blit(fon, (0, 0))
     text_render("Вы проиграли", width // 2, height // 2, BRIGHTRED)
     if record:
-        text_render(f"Новый рекорд: {time_record}", width // 2, height // 4, BRIGHTGREEN)
+        text_render(f"Новый рекорд: {records[levelnumber - 1]}", width // 2, height // 4, BRIGHTGREEN)
     else:
-        text_render(f"Ваше время: {int(time_)}, рекорд: {time_record}", width // 2, height // 4, BRIGHTRED)
+        text_render(f"Ваше время: {int(time_)}, рекорд: {records[levelnumber - 1]}", width // 2, height // 4, BRIGHTRED)
     pygame.display.flip()
     time_ = 0
     while True:
@@ -267,9 +273,10 @@ def score():
 
 
 def gameplay():
-    global phase, hero, time_, all_sprites, level, mousepos, was_paused, time_start, monsta, playerX, playerY, game_start
+    global phase, hero, time_, all_sprites, level, mousepos, was_paused, time_start, monsta, playerX, playerY, \
+                                                                                             game_start, levelnumber
     if not was_paused:
-        loadLevel()
+        loadLevel(levelnumber)
         monsta = list()
     if not hero:
         hero = player.Player(playerX, playerY, character)  # создаем героя по (x, y) координатам
@@ -281,11 +288,11 @@ def gameplay():
     if sound == 1 and not was_paused:
         pygame.mixer.music.load('data_boss_fight.mp3')
         pygame.mixer.music.play(-1)
-    fon = pygame.transform.scale(load_image(BACK[1]), (width, height))
+    fon = pygame.transform.scale(load_image(BACK[levelnumber]), (width, height))
     screen.blit(fon, (0, 0))
-    animated.clear(fon, BACK[1])
-    monsters.clear(fon, BACK[1])
-    all_sprites.clear(fon, BACK[1])
+    animated.clear(fon, BACK[levelnumber])
+    monsters.clear(fon, BACK[levelnumber])
+    all_sprites.clear(fon, BACK[levelnumber])
     pygame.display.update()
     left = right = False  # по умолчанию - стоим
     up = False
@@ -372,8 +379,19 @@ def gameplay():
                 mousepos = e.pos
             if tm - tm_last > 50:
                 tm_last = tm
-                mn = Monster(random.randint(0, total_level_width), random.randint(0, total_level_height),
-                             random.randint(0, 4), random.randint(0, 4), random.randint(0, 150), random.randint(0, 150))
+                while True:
+                    blockfree = 1
+                    mn_x = random.randint(0, total_level_width)
+                    mn_y = random.randint(0, total_level_height)
+                    mn = Monster(mn_x, mn_y, random.randint(1, 4), random.randint(1, 4), random.randint(50, 150),
+                                 random.randint(50, 150))
+                    for p in platforms:
+                        if sprite.collide_rect(mn, p) and mn != p:
+                            blockfree = 0
+                    if ((player.playerX - mn_x) ** 2 + (player.playerY - mn_y) ** 2) ** 0.5 <= 50:
+                        blockfree = 0
+                    if blockfree == 1:
+                        break
                 monsta.append(mn)
                 all_sprites.add(mn)
                 platforms.append(mn)
@@ -394,12 +412,13 @@ def gameplay():
     pygame.display.update()
     time_ += (time_lib.time() - time_start)
 
+
 def hero_choice():
     characters = [['Samus', width // 2 - 200, height // 2], ['Mario', width // 2, height // 2],
                   ['Corvo', width // 2 + 200, height // 2]]
     global sound, phase, new_game, character, mousepos, was_paused
     new_game = 0
-    fon = pygame.transform.scale(load_image(BACK[1]), (width, height))
+    fon = pygame.transform.scale(load_image(BACK[0]), (width, height))
     screen.blit(fon, (0, 0))
     while True:
         pygame.display.flip()
@@ -430,11 +449,15 @@ def hero_choice():
             text_render(i[0], i[1], i[2], GREEN, BRIGHTGREEN, mousepos)
         pygame.display.update()
 
+
 def control_choice(): # Выбор управления
     controls = [['WASD', width // 2 - 100, height // 2], ['Arrows', width // 2 + 100, height // 2]]
-    global sound, phase, new_game, control, mousepos, was_paused
+    global sound, phase, new_game, control, mousepos, was_paused, levelnumber
     new_game = 0
-    fon = pygame.transform.scale(load_image(BACK[1]), (width, height))
+    if was_paused:
+        fon = pygame.transform.scale(load_image(BACK[levelnumber]), (width, height))
+    else:
+        fon = pygame.transform.scale(load_image(BACK[0]), (width, height))
     screen.blit(fon, (0, 0))
     while True:
         pygame.display.flip()
@@ -469,9 +492,43 @@ def control_choice(): # Выбор управления
         text_render('Arrows', width // 2 + 100, height // 2, GREEN, BRIGHTGREEN, mousepos)
         pygame.display.update()
 
+
+def level_choice():
+    global sound, phase, new_game, mousepos, was_paused, levelnumber
+    new_game = 0
+    levels = [[1, width // 2 - 220, height // 2], [2, width // 2, height // 2],
+              [3, width // 2 + 220, height // 2]]
+    fon = pygame.transform.scale(load_image(BACK[0]), (width, height))
+    screen.blit(fon, (0, 0))
+    while True:
+        pygame.display.flip()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                mousepos = event.pos
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button('Назад', width // 2, height - 50, event):
+                    phase = 'start_screen'
+                    return
+                for i in levels:
+                    if button('Уровень ' + str(i[0]), i[1], i[2], event):
+                        levelnumber = i[0]
+                        phase = 'start_screen'
+                        return
+        if sound == 0 and pygame.mixer.music.get_busy():
+            pygame.mixer.music.stop()
+        if sound == 1 and not pygame.mixer.music.get_busy():
+            pygame.mixer.music.play(-1)
+        text_render('Назад', width // 2, height - 50, RED, BRIGHTRED, mousepos)
+        for i in levels:
+            text_render('Уровень ' + str(i[0]), i[1], i[2] - 20, GREEN, BRIGHTGREEN, mousepos)
+            text_render('Рекорд: ' + str(records[i[0] - 1]), i[1], i[2] + 50, GREEN, BRIGHTGREEN, mousepos)
+        pygame.display.update()
+
+
 def pause_menu():
-    global sound, phase, new_game, mousepos, was_paused
-    fon = pygame.transform.scale(load_image(BACK[1]), (width, height))
+    global sound, phase, new_game, mousepos, was_paused, levelnumber
+    fon = pygame.transform.scale(load_image(BACK[levelnumber]), (width, height))
     screen.blit(fon, (0, 0))
     while True:
         pygame.display.flip()
@@ -498,18 +555,18 @@ def pause_menu():
             pygame.mixer.music.stop()
         if sound == 1 and not pygame.mixer.music.get_busy():
             pygame.mixer.music.play(-1)
-        text_render('Продолжить игру', width // 2, height // 2, RED, BRIGHTRED, mousepos)
+        text_render('Продолжить игру', width // 2, height // 2, GREEN, BRIGHTGREEN, mousepos)
         text_render('Выбор управления', width // 2, height // 2 + 150, GREEN, BRIGHTGREEN, mousepos)
-        text_render('Главное Меню', width // 2, height - 50, GREEN, BRIGHTGREEN, mousepos)
+        text_render('Главное Меню', width // 2, height - 50, RED, BRIGHTRED, mousepos)
 
 
 animated = pygame.sprite.Group()  # все анимированные объекты, за исключением героя
 monsters = pygame.sprite.Group()  # Все передвигающиеся объекты
 while True:
     if phase == 'start_screen':
+        all_sprites = pygame.sprite.Group()
         start_screen()
     elif phase == 'score':
-        all_sprites = pygame.sprite.Group()
         score()
     elif phase == 'gameplay':
         gameplay()
@@ -519,3 +576,5 @@ while True:
         control_choice()
     elif phase == 'pause_menu':
         pause_menu()
+    elif phase == 'level_choice':
+        level_choice()
